36bce66731232d634fb7ab0f036fcf5fc393c5a3
nadejda <123@gmail.com>
2021-02-27 21:57:54 +0800

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+let strName = prompt('Введите свое имя')
+console.log('Меня зовут ' + strName);
+let strAge = prompt('Введите свой возраст')
+console.log('Мне ' + strAge + ' лет');
+let strExample1 = prompt('Решите пример 2 + 2 * 2 =')
+console.log('Пример 1 = 6; Ваш ответ = ' + strExample1);
+let strExample2 = prompt('Решите пример 20/4 - 5 =')
+console.log('Пример 2 = 0; Ваш ответ = ' + strExample2);
+let strExample3 = prompt('Решите пример 21%4 =')
+console.log('Пример 3 = 1; Ваш ответ = ' + strExample2);
+alert('Зайдите в консоль разработчика')
